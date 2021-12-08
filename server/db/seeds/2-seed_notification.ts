bb806a7770ad489b469233125f76742747510bc8
Ben Underwood <ben.underwood@oxehealth.com>
2021-12-08 16:58:24 +0000

    change name on notification seed

--- server/db/seeds/2-seed_notification.ts
+++ server/db/seeds/2-seed_notification.ts
@@ -3,21 +3,21 @@ import type { Knex } from 'knex';
 const notifications = [
     {
         typeId: 1,
-        subject: 'room1',
+        subject: 'room 1',
         body: 'nurse call button',
         reminderMinutes: 0,
         updated_at: Date.now() - 60 * 60 * 1000,
     },
     {
         typeId: 1,
-        subject: 'room1',
+        subject: 'room 1',
         body: 'nurse call button',
         reminderMinutes: 0,
         updated_at: Date.now(),
     },
     {
         typeId: 1,
-        subject: 'room2',
+        subject: 'room 2',
         body: 'nurse call button',
         reminderMinutes: 0,
         updated_at: Date.now(),
